7c5533914fd5263d9f5463ab0a52c54863bf951d
Ritesh Parkhi <riteshparkhi9@gmail.com>
2021-06-11 23:55:25 +0530

    Commit in files

--- home.js
+++ home.js
@@ -1,22 +1,5 @@
 // Ritesh Parkhi start
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelectorAll(".nav-link");
-
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
-
 // Ritesh Parkhi end
 
 
